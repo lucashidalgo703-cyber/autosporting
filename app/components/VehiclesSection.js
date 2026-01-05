dddb8fe4596b1f4d71fcc25baf896f19cfc8f21c
lucashidalgo703-cyber <lucashidalgo703@gmail.com>
2026-01-05 16:47:36 -0300

    Update VehiclesSection.js

--- app/components/VehiclesSection.js
+++ app/components/VehiclesSection.js
@@ -4,18 +4,20 @@ import VehicleCard from "./VehicleCard";
 export default function VehiclesSection() {
   return (
     <section className="vehicles">
-      <h2 className="vehicles-title">Vehículos destacados</h2>
+      <div className="vehicles-inner">
+        <h2 className="vehicles-title">Vehículos destacados</h2>
 
-      <div className="vehicles-grid">
-        {vehicles.map((vehicle) => (
-  <VehicleCard
-    key={vehicle.id}
-    id={vehicle.id}
-    title={vehicle.title}
-    price={vehicle.price}
-    image={vehicle.image}
-  />
-))}
+        <div className="vehicles-grid">
+          {vehicles.map((vehicle) => (
+            <VehicleCard
+              key={vehicle.id}
+              id={vehicle.id}
+              title={vehicle.title}
+              price={vehicle.price}
+              image={vehicle.image}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
